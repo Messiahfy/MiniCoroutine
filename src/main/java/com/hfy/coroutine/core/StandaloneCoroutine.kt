ce39e5202814509e5f7b9a609e24c07a8e7b4427
huangfeiyang <huangfeiyang@cvte.com>
2020-08-07 22:54:30 +0800

    0.0.4版本：完成协程的异常处理
    (cherry picked from commit a07b55d16bd71bbb240051cabd1ce27a5f1e4ea5)

--- src/main/java/com/hfy/coroutine/core/StandaloneCoroutine.kt
+++ src/main/java/com/hfy/coroutine/core/StandaloneCoroutine.kt
@@ -1,7 +1,15 @@
 package com.hfy.coroutine.core
 
+import com.hfy.coroutine.exception.CoroutineExceptionHandler
 import kotlin.coroutines.CoroutineContext
 
 class StandaloneCoroutine(context: CoroutineContext) : AbstractCoroutine<Unit>(context) {
 
+    override fun handleJobException(e: Throwable): Boolean {
+        super.handleJobException(e)
+        //从上下文中取出协程异常处理器，处理异常，如果没有，则使用当前线程的未捕获异常处理器
+        context[CoroutineExceptionHandler]?.handleException(context, e)
+            ?: Thread.currentThread().let { it.uncaughtExceptionHandler.uncaughtException(it, e) }
+        return true
+    }
 }
